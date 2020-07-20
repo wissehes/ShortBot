e4adba341e3573c0d1830c4b320c91ff8be08b22
Wisse <wissehes@gmail.com>
2020-07-20 21:24:15 +0200

    Update custom.js

--- commands/custom.js
+++ commands/custom.js
@@ -1,16 +1,19 @@
 const saveLink = require("../modules/savecustomlink")
-const { RichEmbed } = require("discord.js")
+const { MessageEmbed } = require("discord.js")
 
 exports.run = (client, message, args) => {
     const customUrl = args[0]
     args.shift()
     const longUrl = args.join(" ")
-    if(!longUrl){
+    if (!longUrl) {
         return message.reply("You need to provide a URL for me to shorten!")
+    }
+    if (customUrl.length > 10) {
+        return message.reply("Your custom URL can't be longer than 10 characters!")
     }
     saveLink(message.author, longUrl, customUrl)
         .then(data => {
-            const embed = new RichEmbed()
+            const embed = new MessageEmbed()
                 .setTitle("New short link created!")
                 .setColor("GREEN")
                 .setDescription(`**Short link** ${data.shortUrl}`)
@@ -18,11 +21,17 @@ exports.run = (client, message, args) => {
         })
         .catch(err => {
             if (err == "Url already exists") {
-                const embed = new RichEmbed()
+                const embed = new MessageEmbed()
                     .setColor("RED")
                     .setTitle("That URL already exists!")
                     .setDescription("Try again with a different name!")
 
+                message.channel.send(embed)
+            } else {
+                const embed = new MessageEmbed()
+                    .setColor("RED")
+                    .setTitle("An error ocurred!")
+                    .setDescription("Try again later!")
                 message.channel.send(embed)
             }
         })
