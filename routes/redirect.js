cf8c08547839b8054d26a35e6a947801eda81cde
Wisse <wissehes@gmail.com>
2020-07-20 21:26:41 +0200

    create redirect function

--- routes/redirect.js
+++ routes/redirect.js
@@ -3,40 +3,46 @@ const router = express.Router();
 
 const Url = require('../models/Url');
 
+const redirect = async(res, url) => {
+    var clickCount = url.clickCount;
+    clickCount++;
+    await url.updateOne({ clickCount });
+    res.redirect(url.longUrl)
+}
+
 // @route     GET /:code
 // @desc      Redirect to long/original URL
-router.get('/:code', async (req, res) => {
-  console.log(req.session)
-  try {
-    const url = await Url.findOne({ urlCode: req.params.code });
-    if (url) {
-      if(url.secret) {
-        if(!req.session.user) {
-          const config = require('../config')
-          res.render('openPopup', {
-            client_id: config.clientId,
-            redirect: encodeURIComponent(config.callback)
-          })
+router.get('/:code', async(req, res) => {
+    try {
+        const url = await Url.findOne({ urlCode: req.params.code });
+        if (url) {
+            if (url.secret) {
+                if (!req.session.user) {
+                    const config = require('../config')
+                    res.render('openPopup', {
+                        client_id: config.clientId,
+                        redirect: encodeURIComponent(config.callback)
+                    })
+                } else {
+                    if (req.session.user.id === url.userId) {
+                        redirect(res, url)
+                    } else {
+                        res.render('noperms', {
+                            urlCode: url.urlCode,
+                            user: req.session.user
+                        })
+                    }
+                }
+            } else {
+                return redirect(res, url);
+            }
         } else {
-          if(req.session.user.id === url.userId) {
-            res.redirect(301, url.longUrl)
-          } else {
-            res.render('noperms', {
-              urlCode: url.urlCode,
-              user: req.session.user
-            })
-          }
+            return res.status(404).json('No url found');
         }
-      } else {
-        return res.redirect(url.longUrl);
-      }
-    } else {
-      return res.status(404).json('No url found');
+    } catch (err) {
+        console.error(err);
+        res.status(500).json('Server error');
     }
-  } catch (err) {
-    console.error(err);
-    res.status(500).json('Server error');
-  }
 });
 
 module.exports = router;
