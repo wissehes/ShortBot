2a146814ccdaeeb47f1c39f6299b483cbbfee935
Wisse <wissehes@gmail.com>
2020-07-20 21:26:04 +0200

    Add mylinks page

--- routes/index.js
+++ routes/index.js
@@ -7,15 +7,32 @@ const config = require('../config')
 
 // @route     GET /:code
 // @desc      Redirect to long/original URL
-router.get('/', async (req, res) => {
-    //res.redirect(301, 'https://discordapp.com/api/oauth2/authorize?client_id=682159867334623234&permissions=52224&scope=bot')
-    //res.sendFile(__dirname + '/views/index.html')
+
+router.get('/', async(req, res) => {
+    const user = req.session.user ? req.session.user : null
+    const userid = req.session.user ? req.session.user.id : null
+
     res.render('index', {
-        user: req.session.user ? req.session.user : null,
+        user: user,
         client_id: config.clientId,
         redirect: encodeURIComponent(config.callback)
     })
 });
+router.get('/mylinks', async(req, res) => {
+    if (!req.session.user) {
+        return res.redirect("/");
+    }
+    const user = req.session.user
+    const userid = req.session.user.id
+    const urls = await Url.find({ userId: userid }).lean()
+
+    res.render('links', {
+        user: user,
+        client_id: config.clientId,
+        redirect: encodeURIComponent(config.callback),
+        links: urls
+    })
+})
 
 
 module.exports = router;
